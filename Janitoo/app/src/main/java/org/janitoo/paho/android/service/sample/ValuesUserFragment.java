f6567a3f06c1eb8891be047ded51e81fd86ad728
bibi21000 <bibi21000@gmail.com>
2015-11-29 15:22:01 +0100

    Initial import

--- Janitoo/app/src/main/java/org/janitoo/paho/android/service/sample/ValuesUserFragment.java
+++ Janitoo/app/src/main/java/org/janitoo/paho/android/service/sample/ValuesUserFragment.java
@@ -14,25 +14,67 @@ package org.janitoo.paho.android.service.sample;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 /**
  * Fragment for the subscribe pane for the client
  *
  */
-public class ValuesUserFragment extends Fragment {
+public class ValuesUserFragment extends ListFragment {
+  String clientHandle = null;
+  /**
+   * {@link ArrayAdapter} to display the formatted text
+   **/
+  ArrayAdapter<Spanned> arrayAdapter = null;
 
   /**
    * @see Fragment#onCreateView(LayoutInflater, ViewGroup, Bundle)
    */
+  /**
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
+                           Bundle savedInstanceState) {
 
-    return LayoutInflater.from(getActivity()).inflate(R.layout.activity_subscribe, null);
+    return LayoutInflater.from(getActivity()).inflate(R.layout.activity_values_user, null);
+
+  }
+  */
+  /**
+   * @see ListFragment#onCreate(Bundle)
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    //Pull history information out of bundle
+
+    clientHandle = getArguments().getString("handle");
+    Connection connection = Connections.getInstance(getActivity()).getConnection(clientHandle);
+
+    Spanned[] history = connection.history_values_user();
+
+    //Initialise the arrayAdapter, view and add data
+    arrayAdapter = new ArrayAdapter<Spanned>(getActivity(), R.layout.list_view_text_view);
+
+    arrayAdapter.addAll(history);
+    setListAdapter(arrayAdapter);
 
   }
 
+  /**
+   * Updates the data displayed to match the current history
+   */
+  public void refresh() {
+    if (arrayAdapter != null) {
+      arrayAdapter.clear();
+      arrayAdapter.addAll(Connections.getInstance(getActivity()).getConnection(clientHandle).history_values_user());
+      arrayAdapter.notifyDataSetChanged();
+    }
+
+  }
 }
